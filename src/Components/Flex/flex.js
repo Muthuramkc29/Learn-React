905fdef701bcb09a8ca11ab16cb8f525d6d35ecc
Muthuram Kc <you@example.com>
2022-01-19 13:21:46 +0530

    Made Flex Component Responsive

--- src/Components/Flex/flex.js
+++ src/Components/Flex/flex.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import './flex.scss';
+import editorMobile from '../../images/illustration-editor-mobile.svg';
+import laptopMobile from '../../images/illustration-laptop-mobile.svg';
 
 function flex(props) {
     return (
@@ -8,9 +10,9 @@ function flex(props) {
             <div className="py-5">
                 <div className="container--tab position-relative">
                     <h1 className='pt-2 text-center'>{props.title}</h1>
-                    <div className='d-flex justify-content-center align-items-center'>
-                        <div className='w-50 me-2 align-self-start pt-3 mt-3'>
-                            <div className='mt-5 pt-5'>
+                    <div className='content d-flex align-items-center flex-wrap-reverse'>
+                        <div className='content--div align-self-start'>
+                            <div className='pt-5'>
                                 <h1 className='mb-4'>{props.contentTitle1}</h1>
                                 <p className='mt-1'>{props.content1}</p>
                             </div>
@@ -19,29 +21,31 @@ function flex(props) {
                                 <p>{props.content2}</p>
                             </div>
                         </div>
-                        <div className='w-50 h-50'>
-                            <img className='position-absolute start-25 top-0 pb-5' src={props.image} alt='tab' />
+                        <div className=''>
+                            <img className='d-block d-md-none img-fluid pt-3' src={editorMobile} alt='tabMob' />
+                            <img className='d-none d-md-block position-absolute start-25 top-0 pb-5' src={props.image} alt='tab' />
                         </div>
                     </div>
                 </div>
             </div>
             ) : (
-            <div className="py-5 mt-5">
+            <div className="">
                 <div className="container--tab position-relative">
                     <h1 className='pt-2 text-center'>{props.title}</h1>
-                    <div className='d-flex justify-content-end align-items-center'>
-                        <div className='w-50 me-2 align-self-start pt-3 mt-3'>
-                            <div className='w-50 h-50'>
-                                <img className='position-absolute end-50 top-0 pt-5' src={props.image} alt='tab' />
-                            </div>
-                            <div className='mt-5 pt-5'>
+                    <div className='content2 d-flex align-items-center flex-wrap-reverse'>
+                        <div className='content2--div me-2'>
+                            <div className='mt-2'>
                                 <h1 className='mb-4'>{props.contentTitle1}</h1>
                                 <p className='mt-1'>{props.content1}</p>
                             </div>
                             <div className='pt-4'>
-                                <h1 className='mb-4'>{props.contentTitle2}</h1>
+                                <h1 className=''>{props.contentTitle2}</h1>
                                 <p>{props.content2}</p>
                             </div>
+                        </div>
+                        <div className=''>
+                            <img className='d-block d-md-none img-fluid pt-3' src={laptopMobile} alt='tabMob' />
+                            <img className='d-none d-md-block position-absolute end-50 top-0 pt-5' src={props.image} alt='tab' />
                         </div>
                     </div>
                 </div>
