81e02d8b3cff095a01d3c1af3a3abc11013fcff5
Muthuram Kc <you@example.com>
2022-01-23 12:01:43 +0530

    Header and Footer useState() hook used

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import Header from './Components/Header/header';
-import Flex from './Components/Flex/flex';
-import Middle from './Components/Middle/middle';
-import Footer from './Components/Footer/footer';
-import tab from './images/illustration-editor-desktop.svg';
-import phone from './images/illustration-phones.svg';
-import laptop from './images/illustration-laptop-desktop.svg';
+import Header from "./Components/Header/header";
+import Flex from "./Components/Flex/flex";
+import Middle from "./Components/Middle/middle";
+import Footer from "./Components/Footer/footer";
+import tab from "./images/illustration-editor-desktop.svg";
+import phone from "./images/illustration-phones.svg";
+import laptop from "./images/illustration-laptop-desktop.svg";
 
 function App() {
   return (
     <div>
       <Header />
-      <Flex position='right' image={tab} title='Designed for the future' contentTitle1='Introducing an extensible editor' content1='Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog.' contentTitle2='Robust content management' content2='Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized categories, sections, format, or flow. With this opt functionality, you’re in full control.' />
-      <Middle image={phone} title='Designed for the future' contentTitle1='State of the Art Infrastructure' content1='With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.' />
-      <Flex position='left' image={laptop} contentTitle1='Free, open, simple' content1='Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.' contentTitle2='Powerful tooling' content2='Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.' />
+      <Flex
+        position="right"
+        image={tab}
+        title="Designed for the future"
+        contentTitle1="Introducing an extensible editor"
+        content1="Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog."
+        contentTitle2="Robust content management"
+        content2="Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized categories, sections, format, or flow. With this opt functionality, you’re in full control."
+      />
+      <Middle
+        image={phone}
+        title="Designed for the future"
+        contentTitle1="State of the Art Infrastructure"
+        content1="With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. This ensures your site will load instantly, no matter where your readers are, keeping your site competitive."
+      />
+      <Flex
+        position="left"
+        image={laptop}
+        contentTitle1="Free, open, simple"
+        content1="Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn."
+        contentTitle2="Powerful tooling"
+        content2="Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites."
+      />
       <Footer />
     </div>
   );
--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,86 +1,266 @@
-import React from 'react';
-import './header.scss';
-import logo from '../../images/logo.svg';
-import arrow from '../../images/icon-arrow-light.svg';
+import React, { useState } from "react";
+import "./header.scss";
+import logo from "../../images/logo.svg";
 
-function header(props) {
+function Header(props) {
+  const [titleText, setTitleText] = useState("");
+  const [onSubmit, setOnSubmit] = useState(false);
+  const [count, setCount] = useState(0);
+  const [toastDiv, setToastDiv] = useState(() => {
     return (
-        <div class="mainHeader">
-            <div class="container--tab">
-                <div class="navbar navbar-expand-md navbar-dark pt-4 mx-1">
-                    <div class="container-fluid align-items-center">
-                        <a class="navbar-brand">
-                            <span>
-                                <img src={logo} alt="logo" />
-                            </span>
-                        </a>
-                        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div class="collapse navbar-collapse justify-content-start align-items-center" id="main-nav">
-                            <ul class="navbar-nav pt-1">
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDarkDropdownMenuLink"
-                                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Product
-                                    </a>
-                                    <ul class="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
-                                        <li><a class="dropdown-item" href="#">Overview</a></li>
-                                        <li><a class="dropdown-item" href="#">Pricing</a></li>
-                                        <li><a class="dropdown-item" href="#">Marketplace</a></li>
-                                        <li><a class="dropdown-item" href="#">Features</a></li>
-                                        <li><a class="dropdown-item" href="#">Integrations</a></li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDarkDropdownMenuLink"
-                                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Company
-                                    </a>
-                                    <ul class="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
-                                        <li><a class="dropdown-item" href="#">About</a></li>
-                                        <li><a class="dropdown-item" href="#">Team</a></li>
-                                        <li><a class="dropdown-item" href="#">Blog</a></li>
-                                        <li><a class="dropdown-item" href="#">Careers</a></li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDarkDropdownMenuLink"
-                                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Connect
-                                    </a>
-                                    <ul class="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
-                                        <li><a class="dropdown-item" href="#">Contact</a></li>
-                                        <li><a class="dropdown-item" href="#">Newsletter</a></li>
-                                        <li><a class="dropdown-item" href="#">LinkedIn</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div class="d-none d-md-block justify-content-end">
-                            <a class="px-2 mainHeader__login" href="">Login</a>
-                            <a class="px-4 py-2 rounded-pill mainHeader__signup" href="">Sign Up</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="container--fluid container--mod">
-                    <div class="d-flex flex-column justify-content-center align-items-center container__split">
-                        <h1 class="text-center pt-5 mainHeader__title">A modern publishing platform</h1>
-                        <p class="text-center pt-2 mainHeader__subtitle">Grow your audience and build your online brand</p>
-                        <div class="d-flex justify-content-center pt-5 pb-5 gap-3">
-                            <div class="btn rounded-pill py-2 px-3 btn--text">Start for Free</div>
-                            <div class="btn rounded-pill border-white py-2 px-3 btn--white">Learn More</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <div className="d-none">
+        <h1 className="text-center">Toast Display</h1>
+      </div>
     );
+  });
+
+  return (
+    <div class="mainHeader">
+      <div class="container--tab">
+        <div class="navbar navbar-expand-md navbar-dark pt-4 mx-1">
+          <div class="container-fluid align-items-center">
+            <a class="navbar-brand" href="/">
+              <span>
+                <img src={logo} alt="logo" />
+              </span>
+            </a>
+            <button
+              class="navbar-toggler border-0"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#main-nav"
+              aria-controls="main-nav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div
+              class="collapse navbar-collapse justify-content-start align-items-center"
+              id="main-nav"
+            >
+              <ul class="navbar-nav pt-1">
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle text-white"
+                    href="/"
+                    id="navbarDarkDropdownMenuLink"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Product
+                  </a>
+                  <ul
+                    class="dropdown-menu"
+                    aria-labelledby="navbarDarkDropdownMenuLink"
+                  >
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        Overview
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        Pricing
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        Marketplace
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        Features
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        Integrations
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle text-white"
+                    href="/"
+                    id="navbarDarkDropdownMenuLink"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Company
+                  </a>
+                  <ul
+                    class="dropdown-menu"
+                    aria-labelledby="navbarDarkDropdownMenuLink"
+                  >
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        About
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        Team
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        Blog
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        Careers
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle text-white"
+                    href="/"
+                    id="navbarDarkDropdownMenuLink"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Connect
+                  </a>
+                  <ul
+                    class="dropdown-menu"
+                    aria-labelledby="navbarDarkDropdownMenuLink"
+                  >
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        Contact
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        Newsletter
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        LinkedIn
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+
+            <div class="d-none d-md-block justify-content-end">
+              <a class="px-2 mainHeader__login" href="/">
+                Login
+              </a>
+              <a class="px-4 py-2 rounded-pill mainHeader__signup" href="/">
+                Sign Up
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div>{toastDiv}</div>
+        <div class="container--fluid container--mod">
+          <div class="d-flex flex-column justify-content-center align-items-center container__split">
+            <h1 class="text-center mt-5 mainHeader__title">
+              {onSubmit ? titleText : "A modern publishing platform "}
+              <span>{onSubmit ? count : ""}</span>
+            </h1>
+            <div class="d-flex gap-2">
+              <input
+                type="text"
+                className="my-4 p-2"
+                placeholder="Add Title & TitleNo"
+                onChange={(e) => {
+                  setOnSubmit(false);
+                  setTitleText(e.target.value + " ");
+                }}
+              />
+              <div class="d-flex justify-content-center align-items-center gap-2">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => {
+                    setOnSubmit(false);
+                    setCount(count === 0 ? count : count - 1);
+                  }}
+                >
+                  -
+                </button>
+                <p
+                  className="text-white fw-bold mt-3 mx-2"
+                  onChange={(e) => {
+                    setCount(e.target.value);
+                  }}
+                >
+                  {count}
+                </p>
+                <button
+                  className="btn btn-primary "
+                  onClick={() => {
+                    setCount(count + 1 === 11 ? count : count + 1);
+                    setOnSubmit(false);
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={() => {
+                if (titleText === "" && count === 0) {
+                  setToastDiv(() => {
+                    return (
+                      <div className="d-block">
+                        <div className="d-flex justify-content-center">
+                          <div
+                            class="alert alert-warning alert-dismissible fade show"
+                            role="alert"
+                          >
+                            <strong>Warning!</strong> Enter a valid Title
+                            <button
+                              type="button"
+                              class="btn-close"
+                              data-bs-dismiss="alert"
+                              aria-label="Close"
+                            ></button>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  });
+                  setCount("");
+                } else {
+                  setOnSubmit(true);
+                }
+              }}
+            >
+              Submit
+            </button>
+            <p class="text-center pt-2 mainHeader__subtitle">
+              Grow your audience and build your online brand
+            </p>
+            <div class="d-flex justify-content-center pt-5 pb-5 gap-3">
+              <div class="btn rounded-pill py-2 px-3 btn--text">
+                Start for Free
+              </div>
+              <div class="btn rounded-pill border-white py-2 px-3 btn--white">
+                Learn More
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default header;
+export default Header;
